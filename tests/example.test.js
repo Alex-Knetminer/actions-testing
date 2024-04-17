c03bb6e8f91e8bf8de01d3ea835f474219b1b35f
Alex-Knetminer <alex@knetminer.com>
2024-04-17 16:58:37 +0100

    add failing sub test

--- tests/example.test.js
+++ tests/example.test.js
@@ -5,3 +5,7 @@ import { sum } from "../src/utils/sum";
 it('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+it('subs 1 - 2 to equal -1', () => {
+  expect(sub(1, 2)).toBe(-1);
+});
